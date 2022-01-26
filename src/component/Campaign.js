f512715a733be01f5413e31a673bd36f0492f710
mateu52 <wuoelte@gmail.com>
2022-01-26 12:01:42 +0100

    mapujemy porównujemy camp

--- src/component/Campaign.js
+++ src/component/Campaign.js
@@ -33,7 +33,7 @@ function Campaign({users}){
           id: elem.id,
           subject: elem.fields.subject,
           content: elem.fields.content,
-          Subscribers: elem.fields.Subscribers,
+          subscribers: elem.fields.Subscribers,
           status: elem.fields.status
         });
       });
@@ -49,22 +49,46 @@ function Campaign({users}){
       fetchSubscribers();
       
     }, []);
-    
+    let arr = [];
 
+    users.map(name => 
+          camps.map( id =>
+            <div key={id.id, name.fields.name}>
+            {console.log(name.id, id.subscribers)}
+            {
+              id.subscribers.map(sub=>arr.push(sub))
+              //wyniki z tego mapowania wskazac w instrukcji warunkowej poniżej.
+            }
+              {(name.id===id.subscribers) ?
+                arr.push("ok") : arr.push("nok")
+              }
+              </div>
+            )
+      )
+      
+    console.log(arr);
+       
     return(
       <div>
         <h1>Kampanie: </h1>
-        {console.log(camps)}
-        {users && users.map((user) => <div key={user.id}><p>{user.fields.name}</p></div>)}
+        {users && users.map((user) => 
+          <div key={user.id}>
+            <p>{user.fields.name} {user.id} </p>
+          </div>
+          )}
+
+
         {camps && camps.map((sub) =>
-            <div key={sub.id}>
-              <h3>{sub.subject}</h3>
+          <div key={sub.id}>
+              
+              <h3>{sub.subject} dla </h3>
               <p>{sub.content}</p>
-              <p>[{sub.status}]</p>
+              <p>[{sub.status}] {sub.subscribers}</p>
+                          
               
               ___.*.*.*.*.*.*.*.___
             </div>
-            
+          
       )}
 
       </div>
@@ -73,3 +97,5 @@ function Campaign({users}){
 }
   
   export default Campaign;
+
+  
