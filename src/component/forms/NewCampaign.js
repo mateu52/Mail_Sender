ef2384ab97fda7589d73d041c53537bb957d732a
mateu52 <wuoelte@gmail.com>
2022-02-15 18:23:52 +0100

    campaign post CheckName after update

--- src/component/forms/NewCampaign.js
+++ src/component/forms/NewCampaign.js
@@ -12,36 +12,51 @@ function NewCampaign({users}){
 
     const [campDraft, setDraft] =useState();
     const [campSent, setSent ] =useState();
-    const handleSend = data => {
-        setSent( 
+    const [subs, setSubs ] = useState();
+    const handleDraft = data => {
+        setSubs(checkName(data.content));
+        setDraft( 
             {records:[
                 {
                 fields:
                     {
                         "subject":data.subject,    
-                        "content":data.content,
+                        "content":subs+data.content,
                         "status":"sent"
                     }    
                 }]
             }
         );
-        console.log(campSent);
-        api.post('/Campaign', campSent);
+        console.log(campDraft);
+        api.post('/Campaign', campDraft);
     }
+    let result;    
+    function checkName(props){
+        const savers=[];
+        //const result=props;
+        users.map((user)=>{
+            result=props.search(user.fields.name)?user.fields.name:"tak";
+            /* if(result==="nie"){
+                savers.push(user.fields.name);
+            } */
+        })
+
         
-    
-    const handleDraft = data => {
-        
+        return result;
+    }
+    const handleSend = data => {
+       
     }
     return(
         <form>
-                <input {...register("subject" ,{required:true})} />
+                <input placeholder={"czesc {{name}} "}{...register("subject", {required:true})} />
                 {errors.subject && <span>This field is required</span>}
     {/*{name }} wstawianie w możćliwości : select +option z bazy*/}
     {/*chyba że wpisanie {{name}} i przy wysyłaniu bedzie wstawiany odpowiedni użytkownik zbazy*/}
     {/*placeholder={`type your ${name}`}*/}
                 <input {...register("content", {required:true})} />
                 {errors.content && <span>This field is required</span>}
+                {/*Działa po podwójnm kliknięciu*/}
                 <button type="submit" name="draft" onClick={handleSubmit(handleDraft)}>Zapisz</button>
                 <button type="submit" name="send" onClick={handleSubmit(handleSend)}>Wyślij</button>
 
