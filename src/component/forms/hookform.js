82d9491367fc630fb4923c3abf9911b6c4f31b4b
mateu52 <wuoelte@gmail.com>
2022-03-17 19:50:07 +0100

    sending

--- src/component/forms/hookform.js
+++ src/component/forms/hookform.js
@@ -5,14 +5,14 @@ import emailjs from '@emailjs/browser';
 function HookForm(){
     const {register, handleSubmit, formState:{errors} } = useForm();
 
-     const handleSent = (data, e) => {
-        // emailjs.sendForm('gmail89', 'contact_form', data, 'user_HUmnR3VVRTsQyNGd4iT0d')
-        //     .then((result) => {
-        //         console.log(result.text);
-        //     }, (error) => {
-        //         console.log(error.text);
-        //     });
-        console.log(data,e);
+     const handleSent = (data) => {
+         emailjs.sendForm('gmail89', 'contact_form', data, 'user_HUmnR3VVRTsQyNGd4iT0d')
+             .then((result) => {
+                 console.log(result.text);
+             }, (error) => {
+                 console.log(error.text);
+             });
+        console.log(data);
 
     }
 
@@ -30,8 +30,8 @@ function HookForm(){
                     {...register("to_name", {required:true})} />
 
                 <input 
-                    placeholder={"Cześć {{name}} + twój tytuł.... "}
-                    {...register("from_name", {required:true})} />
+                    placeholder={"Podaj email. "}
+                    {...register("To_Email", {required:true})} />
 
                 <input {...register("user_email", {required:true})} />
                     {errors.subject && <span>This field is required</span>}    
